29221ecb7df15774dd2b8a1766b30a635fb6c232
Chitmin <aungchitmin135@gmail.com>
2025-04-27 17:12:54 +0700

    minor update

--- resources/js/pages/access-control.tsx
+++ resources/js/pages/access-control.tsx
@@ -21,27 +21,39 @@ export default function AccessControl({ roles, permissions }: AccessControl.Prop
     return (
         <SidebarLayout title="Access Control">
             <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-                <div className="flex gap-4">
-                    <RoleSelector roles={roles} onValueChange={onRoleChange} />
-                    {activeRole && <SyncButton formName="role-permission-sync" />}
-                </div>
-                <Form {...form}>
-                    <form id="role-permission-sync" onSubmit={form.handleSubmit(onSubmit)}>
-                        <div className="mb-4 grid-cols-2 gap-2 md:grid">
-                            {roles.map((role) =>
-                                permissions.map((permissionsGroup) => (
-                                    <PermissionGroup
-                                        key={`${role.id}-${permissionsGroup.label}`}
-                                        role={role}
-                                        permissionsGroup={permissionsGroup}
-                                        activeRole={activeRole}
-                                        control={form.control}
-                                    />
-                                )),
-                            )}
+                {roles.length > 0 ? (
+                    <>
+                        <div className="flex gap-4">
+                            <RoleSelector roles={roles} onValueChange={onRoleChange} />
+                            {activeRole && <SyncButton formName="role-permission-sync" />}
                         </div>
-                    </form>
-                </Form>
+                        {permissions.length > 0 ? (
+                            <Form {...form}>
+                                <form id="role-permission-sync" onSubmit={form.handleSubmit(onSubmit)}>
+                                    <div className="mb-4 grid-cols-2 gap-2 md:grid">
+                                        {roles.map((role) =>
+                                            permissions.map((permissionsGroup) => (
+                                                <PermissionGroup
+                                                    key={`${role.id}-${permissionsGroup.label}`}
+                                                    role={role}
+                                                    permissionsGroup={permissionsGroup}
+                                                    activeRole={activeRole}
+                                                    control={form.control}
+                                                />
+                                            )),
+                                        )}
+                                    </div>
+                                </form>
+                            </Form>
+                        ) : (
+                            <h2 className="text-center text-xl text-gray-600">There are no permissions! Please register permissions first.</h2>
+                        )}
+                    </>
+                ) : (
+                    <h2 className="text-center text-xl text-gray-600">
+                        There are no roles beside "Super Admin"! Please register roles and permissions first.
+                    </h2>
+                )}
             </div>
         </SidebarLayout>
     );
